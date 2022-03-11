424973b7b5d4a2474cc39f136425d2bf69d4d62f
David Jacka <ahoy.dave@gmail.com>
2022-03-11 11:57:54 +0000

    Add some frills to UI

--- www/index.js
+++ www/index.js
@@ -1,17 +1,16 @@
-import { World, Cell, init, make_panic } from "rust-wasm-tut";
+import * as wasm from "rust-wasm-tut";
 import { memory } from "rust-wasm-tut/rust_wasm_tut_bg";
 
-init();
+wasm.init();
 //make_panic();
 
 const CELL_SIZE = 5;
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 const WORLD_SIZE = 300;
-const TICKS_PER_SECOND = 10;
 const TICKS_PER_FRAME = 10;
 
-const w = World.new_random(WORLD_SIZE, WORLD_SIZE);
+const w = wasm.World.new_random(WORLD_SIZE, WORLD_SIZE);
 const world_width = w.width;
 const world_height = w.height;
 
@@ -35,7 +34,7 @@ let draw_game = () => {
     for (let y = 0; y < world_height; y++) {
         for (let x = 0; x < world_width; x++) {
             let i = x + y * world_width;
-            if (cells[i] === Cell.Alive) {
+            if (cells[i] === wasm.Cell.Alive) {
                 ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
             };
         }
@@ -43,35 +42,55 @@ let draw_game = () => {
 };
 
 const tick_time_element = document.getElementById("tick_time");
-let generation_count = 0;
+const fps_element = document.getElementById("fps");
+const gen_count_element = document.getElementById("generation_count");
 
-const update_world = () => {
-    generation_count++;
-    console.log(`Starting update gen ${generation_count}`);
-    const start = performance.now();
-    for (let i = 0; i < TICKS_PER_FRAME; i++) 
-        w.tick();
-    const end = performance.now();
-    tick_time_element.textContent = `Tick Time: ${Math.round(end - start)}ms`;
-    console.log(`Ending update gen ${generation_count}`)
+const ticks_per_frame_element = document.getElementById("ticks_per_frame_input");
+ticks_per_frame_element.value = TICKS_PER_FRAME;
+let ticks_per_frame = TICKS_PER_FRAME;
+ticks_per_frame_element.onblur = (e) => {
+    let new_tpf = parseInt(e.target.value);
+    if (!isNaN(new_tpf)) {
+        ticks_per_frame = new_tpf;
+        console.log(`Updating ticks per frame to ${new_tpf}`);
+    } else {
+        console.log("new tfp is a NaN");
+        e.value = ticks_per_frame;
+    };
 };
 
-//setInterval(update_world, 1000/TICKS_PER_SECOND);
-
 let play = false;
+const play_button = document.getElementById("play_button");
+play_button.onclick = (e) => {
+    if (play) {
+        play = false;
+        play_button.textContent = "Play";
+    } else {
+        play = true;
+        play_button.textContent = "Stop";
+        requestAnimationFrame(renderLoop);
+    }
+};
 
+let generation_count = 0;
 let last_frame_timestamp = performance.now();
-const fps_element = document.getElementById("fps");
 
-let last_gen = generation_count;
+const update_world = () => {
+    const start = performance.now();
+    for (let i = 0; i < ticks_per_frame; i++) { 
+        w.tick();
+        generation_count++;
+    }
+    const end = performance.now();
+    tick_time_element.textContent = `Update Time: ${Math.round(end - start)}ms`;
+};
 
 const renderLoop = () => {
-    console.log(`Gens per render: ${generation_count - last_gen}`);
-    last_gen = generation_count;
     const now = performance.now();
     const delta = now - last_frame_timestamp;
     last_frame_timestamp = now;
     fps_element.textContent = `FPS: ${Math.round(1000/delta)}`;
+    gen_count_element.textContent = `Generation: ${generation_count}`;
 
     draw_game();
     update_world();
@@ -81,13 +100,3 @@ const renderLoop = () => {
 };
 
 requestAnimationFrame(renderLoop);
-
-const play_button = document.getElementById("play_button");
-play_button.onclick = (e) => {
-    if (play) {
-        play = false;
-    } else {
-        play = true;
-        requestAnimationFrame(renderLoop);
-    }
-};
